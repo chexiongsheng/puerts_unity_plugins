f30ff7051a46cb6f2be96309980c075e8e2c8bea
johnche <johnche@tencent.com>
2020-10-26 11:42:22 +0800

    ios也加上v8_enable_pointer_compression=false

--- v8-build/ios.sh
+++ v8-build/ios.sh
@@ -28,6 +28,7 @@ v8_static_library = true
 ios_enable_code_signing = false
 target_os = "ios"
 target_cpu = "arm64"
+v8_enable_pointer_compression = false
 '
 ninja -C out.gn/arm64.release -t clean
 ninja -C out.gn/arm64.release wee8
