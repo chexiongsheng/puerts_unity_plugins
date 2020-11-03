76110fdc6aa4e92f2b03a315719782d3169d3797
johnche <johnche@tencent.com>
2020-11-03 21:41:19 +0800

    CodeBlocks - Unix Makefiles

--- qjs-build/make_msys2.sh
+++ qjs-build/make_msys2.sh
@@ -1,6 +1,6 @@
 mkdir -p build && cd build
-cmake cmake -G"MSYS Makefiles" ../ 
+cmake cmake -G"CodeBlocks - Unix Makefiles" ../ 
 cd ..
 cmake --build build --config Release
-mkdir -p qjs/Lib/Win64/
-cp build/libqjs.a qjs/Lib/Win64/ 
+mkdir -p ../qjs/Lib/Win64/
+cp build/libqjs.a ../qjs/Lib/Win64/ 
