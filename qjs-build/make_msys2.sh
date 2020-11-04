3b5a8c8ecd14f496c6967cc2cf3b64637b9271bf
johnche <johnche@tencent.com>
2020-11-04 14:47:21 +0800

    换编译器

--- qjs-build/make_msys2.sh
+++ qjs-build/make_msys2.sh
@@ -1,5 +1,5 @@
 mkdir -p build && cd build
-cmake -G"CodeBlocks - Unix Makefiles" -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ../ 
+cmake -G"CodeBlocks - Unix Makefiles" -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ../ 
 cd ..
 cmake --build build --config Release
 mkdir -p ../qjs/Lib/Win64/
