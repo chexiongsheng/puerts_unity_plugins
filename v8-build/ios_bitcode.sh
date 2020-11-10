f5c6fa401a00116a131a377ed047e16759a72cb2
johnche <johnche@tencent.com>
2020-11-10 10:12:52 +0800

    bitcode编译脚本

--- v8-build/ios_bitcode.sh
+++ v8-build/ios_bitcode.sh
@@ -20,7 +20,7 @@ gclient sync
 #cp $GITHUB_WORKSPACE/patch/8.4.371.19/bitcode/BUILD.gn third_party/inspector_protocol/
 
 echo "=====[ Building V8 ]====="
-gn gen $OUTFOLDER --args="v8_enable_pointer_compression=false is_official_build=true use_custom_libcxx=false is_component_build=false symbol_level=0 v8_enable_v8_checks=false v8_enable_debugging_features=false is_debug=false v8_use_external_startup_data=false use_xcode_clang=true enable_ios_bitcode=true v8_enable_i18n_support=false target_cpu=\"arm64\" v8_target_cpu=\"arm64\" target_os=\"ios\" ios_deployment_target=\"9.0\""
+gn gen out.gn/arm64.release --args="v8_enable_pointer_compression=false is_official_build=true use_custom_libcxx=false is_component_build=false symbol_level=0 v8_enable_v8_checks=false v8_enable_debugging_features=false is_debug=false v8_use_external_startup_data=false use_xcode_clang=true enable_ios_bitcode=true v8_enable_i18n_support=false target_cpu=\"arm64\" v8_target_cpu=\"arm64\" target_os=\"ios\" ios_deployment_target=\"9.0\""
 ninja -C out.gn/arm64.release v8_base_without_compiler v8_compiler v8_libplatform v8_libbase v8_libsampler v8_snapshot torque_generated_initializers inspector
 
 #mkdir -p output/v8/Lib/iOS/bitcode
